3fc91b3be9f525beb70af4e39de94334c084c0b4
okeeffed <okeeffe_d@icloud.com>
2017-08-30 14:51:56 +1000

    reducer.js

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -3,11 +3,9 @@ import { routerReducer } from 'react-router-redux'
 
 // reducer-import
 import { HomeReducer } from '../components/home/HomeReducer';
-import { HeaderReducer } from '../components/header/HeaderReducer';
 
 export default combineReducers({
 	// target
 	HomeReducer: HomeReducer,
-	HeaderReducer: HeaderReducer,
 	routing: routerReducer
 });
