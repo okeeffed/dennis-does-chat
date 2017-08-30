07f2991be5da659adf73d4a3fffcb0951eab029e
okeeffed <okeeffe_d@icloud.com>
2017-08-30 14:41:26 +1000

    QuickReplyView.js

--- src/components/quickreply/QuickReplyView.js
+++ src/components/quickreply/QuickReplyView.js
@@ -6,9 +6,8 @@ import PropTypes from 'prop-types';
 class QuickReply extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Ready to roll!</h1>
-				<h2>Time to write some Redux</h2>
+			<div className="quick-reply">
+
 			</div>
 		);
 	}
