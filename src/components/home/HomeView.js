9aede2bfc9f49ad10161bb45ea4704bbc0fb5882
okeeffed <okeeffe_d@icloud.com>
2017-08-30 16:30:04 +1000

    HomeView.js

--- src/components/home/HomeView.js
+++ src/components/home/HomeView.js
@@ -39,6 +39,10 @@ class Home extends Component {
 		});
 
 		message.value = '';
+
+		// Scroll back to bottom
+		const convo = document.querySelector('.conversation-wrapper');
+		convo.scrollTop = convo.scrollHeight;
 	}
 
 	appendConversation() {
