33674365f89375c48b76bba36ff77c355d158fa7
okeeffed <okeeffe_d@icloud.com>
2017-08-30 17:53:06 +1000

    HomeView.js

--- src/components/home/HomeView.js
+++ src/components/home/HomeView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Master from '../master/Master';
+import axios from 'axios';
 /* user imports */
 import QuickReply from '../quickreply/QuickReply';
 import Conversation from '../conversation/Conversation';
