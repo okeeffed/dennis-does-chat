0b0e34f4c6b9bae28861f82d5653e87fc9e3fcc2
okeeffed <okeeffe_d@icloud.com>
2017-08-30 14:47:12 +1000

    ChatBubbleView.js

--- src/components/chatbubble/ChatBubbleView.js
+++ src/components/chatbubble/ChatBubbleView.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 class ChatBubble extends Component {
 	render() {
 		return (
-			<div className=`chat-bubble item -twelve`>
+			<div className="chat-bubble item -twelve">
 				<div className=`reply ${this.props.styling}`>
 					<p>ChatBubble</p>
 				</div>
